7cc5c58795ce84e1a6cdd62bc756d3e7ad3f5311
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2023-10-12 22:12:31 +0530

    :construction: Input added for new folder/file creation

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -2,21 +2,59 @@ import { useState } from "react";
 
 export const Folder = ({ explorer }) => {
   const [expanded, setExpanded] = useState(false);
+  const [showInput, setShowInput] = useState({
+    isVisible: false,
+    isFolder: null,
+  });
 
   const { id, name, isFolder, items } = explorer;
 
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpanded(true);
+    setShowInput({
+      isVisible: true,
+      isFolder,
+    });
+  };
+
   if (isFolder) {
     return (
-      <div id={id} style={{ marginTop: 5, marginLeft: "1rem" }}>
+      <div id={id} style={{ marginTop: 5 }}>
         <div className="folder" onClick={() => setExpanded((prev) => !prev)}>
           <span>📁 {name}</span>
+          <div className="buttons">
+            <button className="btn" onClick={(e) => handleNewFolder(e, false)}>
+              + File
+            </button>
+            <button className="btn" onClick={(e) => handleNewFolder(e, true)}>
+              + Folder
+            </button>
+          </div>
         </div>
 
         <div
           style={{
             display: expanded ? "block" : "none",
+            paddingLeft: "1rem",
           }}
         >
+          {showInput.isVisible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📄"}</span>
+              <input
+                type="text"
+                className="inputContainer__input"
+                autoFocus
+                onBlur={() => {
+                  setShowInput((prevState) => ({
+                    ...prevState,
+                    isVisible: false,
+                  }));
+                }}
+              />
+            </div>
+          )}
           {items.map((exp) => {
             return <Folder explorer={exp} />;
           })}
